9a086d6b5f29b08a8d93646c5103a1a8e6d460bc
Vaibhav Jaiswal <vaibhav.jaiswal3011@gmail.com>
2022-10-23 20:35:31 +0530

    CircularProgressBar.kt designed

--- app/src/main/java/dev/vaibhav/canvasCompose/MainActivity.kt
+++ app/src/main/java/dev/vaibhav/canvasCompose/MainActivity.kt
@@ -3,18 +3,73 @@ package dev.vaibhav.canvasCompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationVector1D
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import dev.vaibhav.canvasCompose.ui.screens.ShapesScreen
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import dev.vaibhav.canvasCompose.ui.components.CircularProgressBar
+import dev.vaibhav.canvasCompose.ui.components.ProgressColors
 import dev.vaibhav.canvasCompose.ui.theme.CanvasComposeTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CanvasComposeTheme {
-                ShapesScreen(modifier = Modifier.fillMaxSize())
+                val scope = rememberCoroutineScope()
+                val progress = remember { Animatable(0f) }
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    CircularProgressBar(
+                        progress = progress.value,
+                        colors = ProgressColors(Color.Red, Color.Yellow, Color.Green),
+                        modifier = Modifier.fillMaxWidth(0.7f).aspectRatio(1f)
+                    )
+                    Spacer(modifier = Modifier.height(32.dp))
+                    AnimationButton(text = "Play") {
+                        scope.launch { animate(progress) }
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    AnimationButton(text = "Play Reverse") {
+                        scope.launch { animate(progress, true) }
+                    }
+                }
             }
         }
+    }
+
+    @Composable
+    private fun AnimationButton(
+        text: String,
+        onClick: () -> Unit
+    ) {
+        OutlinedButton(onClick = onClick) {
+            Text(text = text, style = MaterialTheme.typography.titleMedium)
+        }
+    }
+
+    private suspend fun animate(
+        progress: Animatable<Float, AnimationVector1D>,
+        reverse: Boolean = false
+    ) {
+        val start = if (!reverse) 0f else 1f
+        val end = 1 - start
+        progress.snapTo(start)
+        progress.animateTo(end, animationSpec = tween(10 * 1000, easing = FastOutSlowInEasing))
     }
 }
---
+++ app/src/main/java/dev/vaibhav/canvasCompose/ui/components/CircularProgressBar.kt
@@ -0,0 +1,45 @@
+package dev.vaibhav.canvasCompose.ui.components
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+data class ProgressColors(
+    val start: Color,
+    val mid: Color,
+    val end: Color
+)
+
+@Composable
+fun CircularProgressBar(
+    progress: Float,
+    colors: ProgressColors,
+    modifier: Modifier = Modifier
+) {
+    val color = animateColorAsState(
+        targetValue = progress.getColorFromProgress(colors),
+        animationSpec = tween(2000, easing = FastOutSlowInEasing)
+    )
+    val angle = 360f * progress
+    Canvas(modifier = modifier) {
+        arc(
+            color = color.value,
+            offset = Offset(0f, 0f),
+            angle = angle,
+            strokeWidth = 24.dp.toPx(),
+            size = size
+        )
+    }
+}
+
+private fun Float.getColorFromProgress(color: ProgressColors) = when {
+    this < .4 -> color.start
+    this < .75 -> color.mid
+    else -> color.end
+}
